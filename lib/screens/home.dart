d3398be3434bc0daadcc485bdc1229c04a5532de
Dana18-Mohammed <danamoha2002@gmail.com>
2023-07-24 00:50:37 +0300

    quizz commit

---
+++ lib/screens/home.dart
@@ -0,0 +1,192 @@
+import 'dart:typed_data';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import '../Network_helper/network_helper.dart';
+import 'package:flutter/material.dart';
+import 'package:screenshot/screenshot.dart';
+import 'package:share/share.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  final ScreenshotController _screenshotController = ScreenshotController();
+
+  String imageUrl = '';
+  String content = '';
+  List<dynamic> tags = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchDataAndImage();
+  }
+
+  Future<void> fetchDataAndImage() async {
+    await fetchDataQuote();
+    await fetchImage();
+  }
+
+  Future<void> fetchDataQuote() async {
+    Map<String, dynamic> data = await NetworkHelper().getQuoteData();
+
+    setState(() {
+      content = data['content'] ?? 'Failed to fetch content.';
+      tags = data['tags'] ?? [];
+    });
+    print('quote');
+  }
+
+  Future<void> fetchImage() async {
+    print('image');
+    String apiUrl = 'https://random.imagecdn'
+        '.app/v1/image?width=500&height=550&category=buildings&format=json';
+    if (tags.isNotEmpty) {
+      apiUrl = apiUrl.replaceAll('buildings', tags[0].toString());
+      String url = await NetworkHelper().getImage(apiUrl);
+      setState(() {
+        imageUrl = url ?? 'Failed to fetch image.';
+      });
+    }
+  }
+
+  void refreshData() {
+    setState(() {
+      imageUrl = '';
+      content = '';
+      tags = [];
+    });
+    fetchDataAndImage();
+  }
+
+  // Future<void> takeScreenshotAndShare() async {
+  //   try {
+  //     Future<Uint8List> boundary =
+  //         _screenshotController.captureFromWidget(Screenshot(
+  //       controller: _screenshotController,
+  //       child: const Scaffold(
+  //         body: Center(
+  //           child: HomeScreen(), // Replace with your HomeScreen widget here
+  //         ),
+  //       ),
+  //     ));
+  //
+  //     ui.Image image = await boundary.toImage();
+  //     ByteData byteData =
+  //         await image.toByteData(format: ui.ImageByteFormat.png);
+  //     Uint8List uint8List = byteData.buffer.asUint8List();
+  //
+  //     // Share the screenshot with other apps
+  //     await Share.shareFiles(['screenshot.png'],
+  //         bytesOfFile: uint8List, text: 'Check out this screenshot!');
+  //   } catch (e) {
+  //     print('Error capturing screenshot and sharing: $e');
+  //   }
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Screenshot(
+      controller: _screenshotController,
+      child: Scaffold(
+        body: RepaintBoundary(
+          key: _scaffoldKey,
+          child: Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(imageUrl),
+                    fit: BoxFit.cover,
+                    colorFilter: ColorFilter.mode(
+                        Colors.white.withOpacity(0.8), BlendMode.dstATop),
+                  ),
+                ),
+                constraints: const BoxConstraints.expand(),
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 1, sigmaY: 1),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          colors: [
+                            Colors.white.withOpacity(0.2),
+                            Colors.white.withOpacity(0.2)
+                          ],
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter),
+                    ),
+                  ),
+                ),
+              ),
+              Column(
+                children: <Widget>[
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 290),
+                    child: FloatingActionButton(
+                      onPressed: refreshData,
+                      tooltip: 'Refresh',
+                      backgroundColor: Colors.blueGrey,
+                      child: const Icon(Icons.refresh),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 100,
+                  ),
+                  const Text(
+                    '\u201c',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 70,
+                        color: Colors.black54),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 28),
+                    child: Text(
+                      content,
+                      style: const TextStyle(
+                          fontSize: 32, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 48,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: tags.length,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.only(left: 280),
+                          child: Chip(
+                            label: Text(tags[index]),
+                            labelStyle: const TextStyle(color: Colors.black54),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // takeScreenshotAndShare();
+          },
+          tooltip: 'Take Screenshot and Share',
+          child: const Icon(Icons.share),
+        ),
+      ),
+    );
+  }
+}
+//
